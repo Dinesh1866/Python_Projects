fbef8814fd0656c6068633d05cf71cb7ee42a112
Dinesh1866 <dineshkumarsrds1999@gmail.com>
2022-07-03 07:45:59 +0530

    Brand Name Generator

---
+++ day_01.py
@@ -0,0 +1,18 @@
+#Brand name Generator - gets in city and pet name
+#and then add them to make a Brand name
+
+
+#this is to understand input and conncatination
+
+print("'Welcome to Brand Name Generator'")
+cityName = input("What's name of the city you grew up in?\n")
+petName = input("What's your pet name?\n")
+print("Your Brand name could be : "+cityName+" "+petName)
+
+#eg: input could be - city:CBE and petname:parrot  -> o/p: CBE parrot
+
+#or
+print("'Welcome to Brand Name Generator'")
+cityName = input("What's name of the city you grew up in?\n")
+petName = input("What's your pet name?\n")
+print("Your Brand name could be :",cityName,petName,end=" ")
