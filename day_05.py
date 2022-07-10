68cbacd66337078beb7839ffd8abbca798cab433
Dinesh1866 <dineshkumarsrds1999@gmail.com>
2022-07-10 11:54:13 +0530

    Password generator

--- day_05.py
+++ day_05.py
@@ -12,6 +12,7 @@
 #Hard Level - Order of characters randomised:
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P'''
 
+#Easy level
 import random
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -21,3 +22,18 @@ print("Welcome to the PyPassword Generator!")
 nr_letters= int(input("How many letters would you like in your password?\n")) 
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+letter_choosen =""
+numbers_choosen =""
+symbols_choosen =""
+for letter in range(nr_letters):
+      letter_lst = letters[random.randint(0,len(letters))]
+      letter_choosen +=letter_lst
+for num in range(nr_numbers):
+      num_lst = numbers[random.randint(0,len(numbers))]
+      numbers_choosen +=num_lst
+for symbol in range(nr_symbols):
+      symbols_lst = symbols[random.randint(0,len(symbols))]
+      symbols_choosen +=symbols_lst
+
+print(letter_choosen+symbols_choosen+numbers_choosen)
---
+++ day_06.py
@@ -0,0 +1,8 @@
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+letter_choosen = ""
+for letter in range(int(input())):
+      letter_lst = letters[random.randint(0,len(letters))]
+      letter_choosen +=letter_lst
+print(letter_choosen)
